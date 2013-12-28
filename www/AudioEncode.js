f063a16efb3c5d9151778cacac9cdae156fecd46
wootwoot1234 <Tom@toms-mac.lan>
2013-12-28 11:44:05 +0800

    Update to work with phonegap cli

--- www/AudioEncode.js
+++ www/AudioEncode.js
@@ -2,18 +2,19 @@
 //  AudioEncode.js
 //
 //  Created by Lyle Pratt, September 2011.
-//    Updated Opt 2012 by Keenan Wyrobek for Cordova 2.1.0
+//  Updated Opt 2012 by Keenan Wyrobek for Cordova 2.1.0
+//  Updated by Tomas Krones, January 2014 for Cordova 3.3.0
 //  MIT licensed
 //
 
+// This class converts audio at a file path to M4A format
 
- // This class converts audio at a file path to M4A format
+var exec = require("cordova/exec");
 
- window.encodeAudio = function(originalSrc, successCallback, failCallback) {
+function AudioEncode() {}
 
-     cordova.exec(successCallback, 
-                  failCallback, 
-                  "AudioEncode",
-                  "encodeAudio", 
-                  [originalSrc]);
- };
+AudioEncode.prototype.encodeAudio = function(originalSrc, successCallback, failCallback) {
+    return exec(successCallback, failCallback, "AudioEncode", "encodeAudio", [originalSrc]);
+};
+
+module.exports = new AudioEncode();
